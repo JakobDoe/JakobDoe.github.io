8ee99e83a6c19e368090957385d8bfc23e4786df
JakobDoe <45392305+JakobDoe@users.noreply.github.com>
2021-03-31 22:53:49 +0200

    new commit

--- click_handler.js
+++ click_handler.js
@@ -11,7 +11,25 @@ AFRAME.registerComponent('markerhandler', {
           if (aEntity && intersectedElement === aEntity) {
             window.location = "http://www.google.com/";
 
-            
+
+          }
+      });
+}});
+
+
+AFRAME.registerComponent('markerhandler', {
+
+  init: function() {
+      const animatedMarker = document.querySelector("#animated-marker");
+      const aEntity = document.querySelector("#animated-picture");
+
+      // every click, we make our model grow in size :)
+      animatedMarker.addEventListener('click', function(ev, target){
+          const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+          if (aEntity && intersectedElement === aEntity) {
+            window.location = "http://www.google.com/";
+
+
           }
       });
 }});
