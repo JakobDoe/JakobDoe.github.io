c72dc86c862065226485fbc3eed857f1f3396f33
JakobDoe <45392305+JakobDoe@users.noreply.github.com>
2021-03-31 23:26:34 +0200

    asdasdf

--- click_handler.js
+++ click_handler.js
@@ -4,14 +4,16 @@ AFRAME.registerComponent('markerhandler', {
           init: function() {
               const animatedMarker = document.querySelector("#animated-marker");
               const aEntity = document.querySelector("#animated-model");
+              cosnt newEntity = document.querySelector("#animated-text");
 
               // every click, we make our model grow in size :)
               animatedMarker.addEventListener('click', function(ev, target){
                   const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
                   if (aEntity && intersectedElement === aEntity) {
                     window.location = "https://stackoverflow.com/questions/6452502/adding-click-event-handler-to-iframe";
-
-
+                  }
+                  if (newEntity && intersectedElement === newEntity) {
+                    window.location = "https://google.com";
                   }
               });
         }
