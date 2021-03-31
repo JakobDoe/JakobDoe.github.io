0375f72bc889ff1ee1b712c6345bdd1c34818cc2
JakobDoe <45392305+JakobDoe@users.noreply.github.com>
2021-03-31 23:27:04 +0200

    Update click_handler.js

--- click_handler.js
+++ click_handler.js
@@ -4,7 +4,7 @@ AFRAME.registerComponent('markerhandler', {
           init: function() {
               const animatedMarker = document.querySelector("#animated-marker");
               const aEntity = document.querySelector("#animated-model");
-              cosnt newEntity = document.querySelector("#animated-text");
+              const newEntity = document.querySelector("#animated-text");
 
               // every click, we make our model grow in size :)
               animatedMarker.addEventListener('click', function(ev, target){
