e95d5e516c445d8fe9141f2fe99ac56c9f841c59
JakobDoe <45392305+JakobDoe@users.noreply.github.com>
2021-03-31 23:02:17 +0200

    Update click_handler.js

--- click_handler.js
+++ click_handler.js
@@ -1,35 +1,33 @@
 
 AFRAME.registerComponent('markerhandler', {
 
-  init: function() {
-      const animatedMarker = document.querySelector("#animated-marker");
-      const aEntity = document.querySelector("#animated-model");
+          init: function() {
+              const animatedMarker = document.querySelector("#animated-marker");
+              const aEntity = document.querySelector("#animated-model");
 
-      // every click, we make our model grow in size :)
-      animatedMarker.addEventListener('click', function(ev, target){
-          const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-          if (aEntity && intersectedElement === aEntity) {
-            window.location = "http://www.google.com/";
+              // every click, we make our model grow in size :)
+              animatedMarker.addEventListener('click', function(ev, target){
+                  const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+                  if (aEntity && intersectedElement === aEntity) {
+                    window.location = "https://stackoverflow.com/questions/6452502/adding-click-event-handler-to-iframe";
 
 
+                  }
+              });
+        }
+
+        function iframeclick() {
+          document.getElementById("theiframe").contentWindow.document.body.onclick = function() {
+            window.location = "http://www.floral.at/";
+              }
           }
-      });
-}});
 
 
-AFRAME.registerComponent('markerhandler', {
-
-  init: function() {
-      const animatedMarker = document.querySelector("#animated-marker");
-      const aEntity = document.querySelector("#animated-picture");
-
-      // every click, we make our model grow in size :)
-      animatedMarker.addEventListener('click', function(ev, target){
-          const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-          if (aEntity && intersectedElement === aEntity) {
-            window.location = "http://www.google.com/";
 
 
-          }
-      });
-}});
+
+
+
+});
+
+
