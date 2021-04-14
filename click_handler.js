af4ecf6e5342f82eea16b170e90e8d7f7ffecd72
JakobDoe <45392305+JakobDoe@users.noreply.github.com>
2021-04-14 13:43:10 +0200

    new click second try

--- click_handler.js
+++ click_handler.js
@@ -1,38 +1,38 @@
 
-AFRAME.registerComponent('markerhandler', {
+// AFRAME.registerComponent('markerhandler', {
 
-          init: function() {
-              const animatedMarker = document.querySelector("#animated-marker");
-              const aEntity = document.querySelector("#animated-model");
-              const aModel = document.querySelector("#animated-text");
-              const aPhoneIcon = document.querySelector("#phoneicon");
-              const aPhoneNumber = document.querySelector("#phonenumber");
+//           init: function() {
+//               const animatedMarker = document.querySelector("#animated-marker");
+//               const aEntity = document.querySelector("#animated-model");
+//               const aModel = document.querySelector("#animated-text");
+//               const aPhoneIcon = document.querySelector("#phoneicon");
+//               const aPhoneNumber = document.querySelector("#phonenumber");
       
               
 
-              // every click, we make our model grow in size :)
-              animatedMarker.addEventListener('click', function(ev, target){
-                  const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-                  if (aEntity && intersectedElement === aEntity) {
-                    window.location = "http://google.com";
-                  }
-                  if (aModel && intersectedElement === aModel) {
-                    window.location = "http://floral.at";
-                  }
-                  if (aPhoneIcon && intersectedElement === aPhoneIcon) {
-                    document.location.href = "tel:00436761234567";
-                  }
-                  if (aPhoneNumber && intersectedElement === aPhoneNumber) {
-                    document.location.href = "tel:00436761234567";
-                  }
-              });
-        }
+//               // every click, we make our model grow in size :)
+//               animatedMarker.addEventListener('click', function(ev, target){
+//                   const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+//                   if (aEntity && intersectedElement === aEntity) {
+//                     window.location = "http://google.com";
+//                   }
+//                   if (aModel && intersectedElement === aModel) {
+//                     window.location = "http://floral.at";
+//                   }
+//                   if (aPhoneIcon && intersectedElement === aPhoneIcon) {
+//                     document.location.href = "tel:00436761234567";
+//                   }
+//                   if (aPhoneNumber && intersectedElement === aPhoneNumber) {
+//                     document.location.href = "tel:00436761234567";
+//                   }
+//               });
+//         }
 
 
        
 
 
-});
+// });
 
 AFRAME.registerComponent('navigate-on-click', {
   schema: {
